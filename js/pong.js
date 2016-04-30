74ab5a20c704388ba119d723d304bdd62bb11758
linuxrules94 <coolspeedy6@gmail.com>
2016-04-30 09:13:34 -0400

    Actually Disabled debuging this time.

--- js/pong.js
+++ js/pong.js
@@ -544,7 +544,7 @@ var Pong = function()
     $('body').click(function (event) {
 	// Keep our menu open if user clicks on the webpage's menu
 	// NOTE:  (might make separate css selector like "pong-keep-open" or "pong-ignore" as well)
-        console.debug('Body click: ', event, 'on: ', event.target, 'Interactive: ', YaP_Object.Settings.Interactive);
+        //console.debug('Body click: ', event, 'on: ', event.target, 'Interactive: ', YaP_Object.Settings.Interactive);
 	if ($(event.target).is('a, button, span')) return;
 	if (YaP_Object.Settings.Menu) {
 	    YaP_Object.Functions.toggleMenu();
